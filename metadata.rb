5b677a3a000e1e2600bdde6eeb730291e00e416d
Aaron Addleman <aaronaddleman@gmail.com>
2015-07-02 12:30:50 -0700

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'base'
+maintainer       'gplv2'
+maintainer_email 'aaronaddleman@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures base'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: base
+# Recipe:: default
+#
+# Copyright 2015, gplv2
+#
+# All rights reserved - Do Not Redistribute
+#
